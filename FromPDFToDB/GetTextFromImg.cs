02d15d6e5491cc142b434faa50f08baa0ddb9ee4
AliceOiseau <angry_bird_@ukr.net>
2017-08-17 23:01:56 +0300

    added Tesseract

---
+++ FromPDFToDB/GetTextFromImg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace FromPDFToDB
+{
+	public static class GetTextFromImg
+	{
+		public static string GetText(Bitmap imgsource)
+		{
+			var ocrtext = string.Empty;
+			using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+			{
+				using (var img = PixConverter.ToPix(imgsource))
+				{
+					using (var page = engine.Process(img))
+					{
+						ocrtext = page.GetText();
+					}
+				}
+			}
+
+			return ocrtext;
+		}
+	}
+}
---
+++ FromPDFToDB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromPDFToDB
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Bitmap bimage = new Bitmap(@"C:\Solomiya\K&K Uni Ftwr PYG OCT 12 209417.bmp");
+			Console.WriteLine(GetTextFromImg.GetText(bimage));
+			Console.Read();
+		}
+	}
+}
